6227831ce442a4474849325339b5dab03c938e59
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-02-26 14:51:25 +0100

    [Fix #1479] Validate email format with Rails format validation

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,9 +1,12 @@
 class Invite < ActiveRecord::Base
+  include EmailSanitizableConcern
+
   belongs_to :dossier
   belongs_to :user
 
+  before_validation -> { sanitize_email(:email) }
+
   validates_presence_of :email
   validates_uniqueness_of :email, :scope => :dossier_id
-
-  validates :email, email_format: true
+  validates :email, format: { with: Devise.email_regexp, message: "n'est pas valide" }, allow_nil: true
 end
